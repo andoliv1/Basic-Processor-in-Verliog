818107a3edd76d8b00264407ea5a7d5b3a16f485
Francisco De Melo Oliveira <andoliv@signals9.bu.edu>
2019-11-10 17:33:31 -0500

    fixed errors

--- decode.v
+++ decode.v
@@ -93,31 +93,195 @@ assign write_sel = instruction[11:7];
  assign i_imm_orig= instruction[31:20];
  assign uj_imm={instruction[31],instruction[21:12],instruction[22],instruction[30:23]};
  assign sb_imm_orig={instruction[31],instruction[7],instruction[30:25],instruction[11:8]};
+ 
+ assign s_imm_32={{20{s_imm_msb[6]}},s_imm_msb,s_imm_lsb};
+ assign u_imm_32={{12{u_imm[19]}},u_imm};
+ assign i_imm_32={{20{i_imm_orig[11]}},i_imm_orig};
+ assign uj_imm={{12{uj_imm[19]}},uj_imm};
+ assign sb_imm_32={{20{sb_imm_orig[11]}},sb_imm_orig};
 
+reg next_PC_select_1,branch_op_1,wEn_1,wb_sel_1,mem_wEn_1,op_B_sel_1;
+reg[1:0] op_A_sel_1;
+reg[31:0] imm32_1;
+reg[5:0] ALU_Control_1;
 always@(*)
 begin 
 case(opcode)
-    R_TYPE:
-        begin
-        next_PC_select=0;
-        branch_op=0;
-        wEn=1;
-        mem_wEn=0;
-        op_A_sel=2'b00;
-        op_B_sel=0;
-        if(funct3==3'b000)
-        begin 
-            if(funct7==7'b0)
-            ALU_Control=6'b0;
+   R_TYPE:
+   begin
+       next_PC_select_1=0;
+       branch_op_1=0;
+       wEn_1=1;
+       wb_sel_1=0;
+       mem_wEn_1=0;
+       op_A_sel_1=2'b00;
+       op_B_sel_1=0;
+       imm32_1=32'b0;
+       if(funct3==3'b000)
+       begin
+           if(funct7==7'b0)
+           ALU_Control_1=6'b0;//add
+           else
+           ALU_Control_1=6'b001000;//sub
+       end
+       else if(funct3==3'b111)
+           ALU_Control_1=6'b000111;//and
+       else if(funct3==3'b110)
+           ALU_Control_1=6'b000110;//or
+       else if(funct3==3'b010)
+           ALU_Control_1=6'b000010;//slt
+       else if(funct3==3'b001)
+           ALU_Control_1=6'b000001;//sll
+       else if(funct3==3'b011)
+          ALU_Control_1=6'b000010;//sltu
+       else if(funct3==3'b100)
+           ALU_Control_1=6'b000100;//xor
+       else if(funct3==3'b101)
+       begin
+           if(funct7==7'b0)
+           ALU_Control_1=6'b000101;//srl
+           else
+           ALU_Control_1=6'b001101;//sra
+       end
+   end
+   I_TYPE:
+   begin
+       next_PC_select_1=0;
+       branch_op_1=0;
+       wEn_1=1;
+       wb_sel_1=0;
+       mem_wEn_1=0;
+       op_A_sel_1=2'b00;
+       op_B_sel_1=1;//make sure if 1 or 0
+       imm32_1=i_imm_32;
+       if(funct3==3'b000)
+           ALU_Control_1=6'b0;
+       else if(funct3==3'b111)
+           ALU_Control_1=6'b000111;//andi
+       else if(funct3==3'b110)
+           ALU_Control_1=6'b000110;//ori
+       else if(funct3==3'b010)
+           ALU_Control_1=6'b000010;//slti
+       else if(funct3==3'b001)
+           ALU_Control_1=6'b000001;//slli (figure out shamt)
+       else if(funct3==3'b011)
+          ALU_Control_1=6'b000010;//sltiu
+       else if(funct3==3'b100)
+           ALU_Control_1=6'b000100;//xori
+       else if(funct3==3'b101)
+               begin
+                   if(funct7==7'b0)
+                   ALU_Control_1=6'b000101;//srli(figure out shamt)
+                   else
+                   ALU_Control_1=6'b001101;//srai(figure out shamt)
+                   end
+      end
+      STORE:
+      begin
+           next_PC_select_1=0;
+           branch_op_1=0;
+           wEn_1=0;
+           wb_sel_1=0;
+           mem_wEn_1=1;
+           imm32_1=s_imm_32;
+           op_A_sel_1=2'b00;
+           op_B_sel_1=1;//make sure if 1 or 0
+           ALU_Control_1=6'b0;  
+      end
+      LOAD:
+         begin
+              next_PC_select_1=0;
+              branch_op_1=0;
+              wEn_1=1;
+              wb_sel_1=1;
+              mem_wEn_1=0;
+              imm32_1=s_imm_32;
+              op_A_sel_1=2'b00;
+              op_B_sel_1=1;//make sure if 1 or 0
+              ALU_Control_1=6'b0;  
+         end
+        BRANCH:
+            begin
+            next_PC_select_1=1;
+            branch_op_1=1;
+            wEn_1=0;
+            wb_sel_1 = 0;
+            mem_wEn_1 = 0;
+            op_A_sel_1 = 2'b00;
+            op_B_sel_1 = 1'b0;
+            imm32_1 = sb_imm_32;
+            if(funct3 == 3'b000)
+                ALU_Control_1 = 6'b010000;
+            else if(funct3 == 3'b001)
+                ALU_Control_1= 6'b010001;
+            else if(funct3 == 3'b100)
+                ALU_Control_1 = 6'b010100;
+            else if(funct3 == 3'b101)
+                ALU_Control_1= 6'b010101;
+            else if(funct3 == 3'b110)
+                ALU_Control_1= 6'b010110;
             else
-            ALU_C
-            
-         
-        end
-    
-    
-/******************************************************************************
-*                      Start Your Code Here
-******************************************************************************/
+                ALU_Control_1 = 6'b010111;
+            end  
+          JALR:
+            begin
+            next_PC_select_1 = 1;
+            branch_op_1 = 0;
+            wEn_1 = 1;
+            wb_sel_1 = 1;
+            mem_wEn_1 = 0;
+            op_A_sel_1 = 2'b01;
+            op_B_sel_1 = 1;
+            imm32_1 = uj_imm_32;
+            ALU_Control_1 = 6'b111111;
+            end
+          JAL:
+            begin
+            next_PC_select_1 = 1;
+            branch_op_1 = 0;
+            wEn_1 = 0;
+            wb_sel_1 = 0;
+            mem_wEn_1 = 0;
+            op_A_sel_1 = 2'b01;
+            op_B_sel_1 = 1;
+            imm32_1 = uj_imm_32;
+            ALU_Control_1 =6'b011111;
+            end
+          AUIPC:
+            begin
+            next_PC_select_1 = 0;
+            branch_op_1 = 0;
+            wEn_1 = 0;
+            wb_sel_1 = 0;
+            mem_wEn_1 = 0;
+            op_A_sel_1 = 2'b01;
+            op_B_sel_1 = 1;
+            imm32_1 = u_imm_32;
+            ALU_Control_1 = 6'b000000;
+            end
+          LUI:
+            begin
+            next_PC_select_1 = 0;
+            branch_op_1 = 0;
+            wEn_1 = 1;
+            wb_sel_1 = 0;
+            mem_wEn_1 = 0;
+            op_A_sel_1 = 2'b00;
+            op_B_sel_1 = 1;
+            imm32_1 = u_imm_32;
+            ALU_Control_1 = 6'b000000;
+            end
+            endcase
+            end
+
+assign next_PC_select =  next_PC_select_1;
+assign branch_op = branch_op_1;
+assign wEn = wEn_1;
+assign wb_sel = wb_sel_1;
+assign mem_wEn = mem_wEn_1;
+assign op_A_sel= op_A_sel_1;
+assign op_B_sel = op_B_sel_1;
+assign imm32 = imm32_1;
+assign ALU_Control = ALU_Control_1;
 
 endmodule
